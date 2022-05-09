e4002dcede272c5a1449711816a4660e750eb25e
Slava <sh-e-95@mail.ru>
2022-05-10 00:59:42 +0500

    add bottom app bar

--- crypto_manager/lib/pages/home_page.dart
+++ crypto_manager/lib/pages/home_page.dart
@@ -33,24 +33,44 @@ class _HomePageState extends State<MyHomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Currency Converter'), actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () => {
-              showSearch(
-                  context: context, delegate: MySearchDelegates(_currencies))
-            },
-          )
-        ]),
-        body: _isLoading
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: _currencies.length,
-                itemBuilder: (BuildContext context, int index) =>
-                    _getRowWithDivider(index),
-              ));
+      appBar: AppBar(title: const Text('Currency Converter'), actions: [
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () => {
+            showSearch(
+                context: context, delegate: MySearchDelegates(_currencies))
+          },
+        )
+      ]),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: _currencies.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  _getRowWithDivider(index),
+            ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      extendBody: true,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Colors.blue,
+        child: Row(
+          children: [
+            //add_alert_outlined
+            IconButton(
+              icon: const Icon(Icons.favorite_border),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 
   Widget _getRowWithDivider(int i) {
